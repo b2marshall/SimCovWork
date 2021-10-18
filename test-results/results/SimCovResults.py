fixes scaling issues on x-axis of plots. Now xticks in 8-hour increments

test-results/results/SimCovResults.py
@@ -10,8 +10,12 @@ with open(filename) as fptr:
 rows = [x.split() for x in lines[1:]]
 column_names = lines[0][1:] 
 
-times = [int(x[0]) for x in rows]
+times = [int(x[0])/60 for x in rows]
+ticks = []
 
+for element in times: 
+    if element % 8 == 0:
+        ticks.append(element)
 
 incubating_cells = [int(x[1]) for x in rows]
 
@@ -25,7 +29,7 @@ plt.figure(figsize=(9,5))
 plt.plot(times, incubating_cells)
 plt.xlim(0,times[-1])
 plt.ylabel("Number of Incubating Cells")
-plt.xticks(np.arange(len(hours)), tuple(hours)) 
-plt.xlabel("Timesteps After Infection")
+plt.xticks(ticks) 
+plt.xlabel("Hours After Infection")
 plt.title("Incubating Cells") 
 plt.savefig("incubatingcells.png")